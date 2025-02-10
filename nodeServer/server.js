e95f1ff52adc21d25b6d18a09e67caa7f54b2f0f
Jia Hua Jiang <jiahua212@gmail.com>
2025-02-10 14:51:10 -0800

    server.js

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -6,10 +6,31 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
+
+//start game instance here
+
 // Basic route
 app.get("/", (req, res) => {
     res.send("Hello, Node.js server!");
 });
+
+app.get("/step", (req, res) => {
+    //do step stuff
+    //handle game stuff
+    res.send("Game stuff")
+
+});
+
+app.get("/reset", (req, res) => {
+    //do step stuff
+    //handle game stuff
+    res.send("Game stuff")
+
+});
+
+
+
+
 
 // Start server
 app.listen(PORT, () => {
