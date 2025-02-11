bbd15a4134f23b84a9177cd1a4aae6a4657dfad0
Jia Hua Jiang <jiahua212@gmail.com>
2025-02-10 18:01:22 -0800

    node.js-ified snake game

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,10 +1,32 @@
-const express = require("express");
+
+
+import express from "express";
+import Game from "./slither.io/js/game.js"
+import game from "./slither.io/js/game.js";
 const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+let game_H = 0;
+let game_W = 0;
+let SPEED = 1;
+let MaxSpeed = 0;
+// let chX = chY = 1;
+let mySnake = [];
+let FOOD = [];
+let NFood = 2000;
+let Nsnake = 20;
+let sizeMap = 2000;
+let index = 0;
+let minScore = 200;
+let die = false;
+
+var g = new Game(game_W, game_H, SPEED, MaxSpeed, mySnake, FOOD, NFood, 
+    Nsnake, sizeMap, index, minScore, die);
 
 
 //start game instance here
@@ -17,16 +39,22 @@ app.get("/", (req, res) => {
 app.get("/step", (req, res) => {
     //do step stuff
     //handle game stuff
-    res.send("Game stuff")
+    res.send("Game after step")
 
 });
 
 app.get("/reset", (req, res) => {
     //do step stuff
     //handle game stuff
-    res.send("Game stuff")
-
+    res.send("Game after reset")
 });
+
+app.get("/state", (req, res) => {
+    //do step stuff
+    //handle game stuff
+    res.send("Game stuff")
+});
+
 
 
 
@@ -36,3 +64,5 @@ app.get("/reset", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app; // Export the Express app
