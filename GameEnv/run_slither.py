0f204a88a9e07f17d1a7e9cdf1463e3faa4dcd70
Jia Hua Jiang <jiahua212@gmail.com>
2025-02-20 21:28:04 -0800

    working gymEnv parse

--- GameEnv/run_slither.py
+++ GameEnv/run_slither.py
@@ -2,7 +2,29 @@
 
 import gymnasium
 import gymnasium_env
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from gymnasium import spaces
+import numpy as np
+from gymnasium.spaces.utils import flatten_space
+
 env = gymnasium.make('gymnasium_env/slither_world')
+# class FlattenObservationWrapper(gymnasium.ObservationWrapper):
+#     def __init__(self, env):
+#         super().__init__(env)
+#         self.observation_space = spaces.Box(
+#             low=-np.inf, high=np.inf, shape=(10 * 2, ), dtype=np.float32  # Adjust according to flattened shape
+#         )
+
+#     def observation(self, observation):
+#         # Flatten the body part coordinates and add to other features (size, score)
+#         # body_flattened = observation["mySnake"]["body"].flatten()  # Flatten the 2D body array
+#         print("thisssss",flatten_space(observation).shape)
+#         return flatten_space(observation)
+
+
+# env = FlattenObservationWrapper(env)
 observation, info = env.reset()
 
 
@@ -13,14 +35,10 @@ while not episode_over:
 
     episode_over = terminated or truncated
 
-# Ensure observations match the expected space
-# obs_space = env.observation_space
+# Create environment
 
-# obs, _ = env.reset()
+# Instantiate the agent
+model = PPO('MultiInputPolicy', env, verbose=1)
+# Train the agent
+model.learn(total_timesteps=int(2e5))
 
-# # print(obs)
-# # Validate before returning
-# try:
-#     assert env.observation_space.contains(obs), f"ERROR: Observation does not match observation space! Observation: {obs}"
-# except AssertionError as e:
-#     print(e)
